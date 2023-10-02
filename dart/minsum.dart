28a9767a6b05d89b9825b09a24b7835c8c356b2e
dinux <wikiliftapp@gmail.com>
2023-10-02 10:09:56 +0200

    road to 3kyu

--- dart/minsum.dart
+++ dart/minsum.dart
@@ -3,5 +3,5 @@
 int minSum(arr) => (arr..sort()).asMap().entries.fold(0, (sum, e) => sum + e.value * arr[arr.length - e.key - 1]) ~/ 2;
 
 void main() {
-  print(minSum([5, 4, 2, 3])); // Output: 22
+  print(minSum([5, 4, 2, 3]));
 }
